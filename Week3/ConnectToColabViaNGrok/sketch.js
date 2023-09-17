95fe92d342a22e84b24197c44e41db320ca3aacd
Dano <dan.osullivan@nyu.edu>
2023-09-17 17:42:50 -0400

    woods

--- Week3/ConnectToColabViaNGrok/sketch.js
+++ Week3/ConnectToColabViaNGrok/sketch.js
@@ -1,4 +1,4 @@
-const NGrokAddress = "https://dano.ngrok.dev";
+const NGrokAddress = "https://firm-honeybee-noticeably.ngrok-free.app";
 
 const container = document.getElementById("container");
 var input_image_field = document.createElement("input");
@@ -16,9 +16,16 @@ input_image_field.addEventListener("keyup", function (event) {
 async function askForPicture(p_prompt) {
     const imageDiv = document.getElementById("resulting_image");
     imageDiv.innerHTML = "Waiting for reply from Colabs via NGrok...";
+
     let data = {
-        "prompt": p_prompt,
+        "version": "da77bc59ee60423279fd632efb4795ab731d9e3ca9705ef3341091fb989b7eaf",
+        input: {
+            "prompt": p_prompt,
+            "width": 1024,
+            "height": 512,
+        },
     };
+
     console.log("Asking for Picture Info From Colabs via NGrok", data);
     let options = {
         method: "POST",
@@ -27,9 +34,11 @@ async function askForPicture(p_prompt) {
         },
         body: JSON.stringify(data),
     };
+    //const picture_info = await fetch(NGrokAddress + "/dummypath/", options);
     const picture_info = await fetch(NGrokAddress + "/generateIt/", options);
-    //console.log("picture_response", picture_info);
+    console.log("picture_response", picture_info);
     const result = await picture_info.json();
+    console.log("json", result);
     imageDiv.innerHTML = "";
     let img = document.createElement("img");
     img.src = "data:image/jpeg;base64," + result.b64Image;
---
+++ Week4/PreplicateInPaintP5/sketch.js
@@ -0,0 +1,87 @@
+let img;
+let button;
+let inputBox;
+let mask;
+let maskBase64 = "";
+let canvas; const replicateProxy = "https://replicate-api-proxy.glitch.me"
+
+let canvas2;
+
+function preload() {
+  img = loadImage("jacob.jpeg");
+}
+function setup() {
+  button = createButton("Ask");
+  button.mousePressed(ask);
+  button.position(530, 40);
+  inputBox = createInput("Old Man");
+  inputBox.position(530, 10);
+
+  canvas = createCanvas(512, 512);
+  canvas.position(0, 0);
+
+  mask = createGraphics(512, 512);
+  mask.fill(0, 0, 0);
+  mask.noStroke();
+  image(img, 0, 0, 512, 512);
+
+}
+
+function draw() {
+  //nothing
+}
+
+function mouseDragged() {
+  mask.noStroke();
+  mask.fill(255, 255, 255);
+  mask.ellipse(mouseX, mouseY, 10, 10);
+  ellipse(512 + mouseX, mouseY, 10, 10);
+  image(mask, 0, 0, 512, 512);
+
+}
+function mouseReleased() {
+  maskBase64 = mask.elt.toDataURL();
+}
+
+async function ask() {
+  image(img, 0, 0, 512, 512);
+  canvas.loadPixels();
+  mask.loadPixels();
+  let maskBase64 = mask.elt.toDataURL();
+  let imgBase64 = canvas.elt.toDataURL();
+
+
+  let postData = {
+    "version": "8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f",
+    input: {
+      "prompt": inputBox.value(),
+      "width": 512,
+      "height": 512,
+      "prompt_strength": 0.5,
+      "image": imgBase64,
+      "mask": maskBase64,
+
+    },
+  };
+
+  let url = replicateProxy + "/create_n_get";
+  const options = {
+    headers: {
+      "Content-Type": `application/json`,
+    },
+    method: "POST",
+    body: JSON.stringify(postData), //p)
+  };
+  console.log("Asking for Picture ", url, options);
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result.output[0]);
+
+  loadImage(result.output[0], function (newImage) {
+    console.log("image loaded", newImage);
+    mask = createGraphics(512, 512);
+    img = newImage;
+    image(img, 0, 0, 512, 512);
+  });
+
+}
