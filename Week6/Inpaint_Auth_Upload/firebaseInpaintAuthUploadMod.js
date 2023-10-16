470538bbbda9734995764e9940962cd8a662e798
Dano <dan.osullivan@nyu.edu>
2023-10-16 16:29:16 -0400

    home

--- Week6/Inpaint_Auth_Upload/firebaseInpaintAuthUploadMod.js
+++ Week6/Inpaint_Auth_Upload/firebaseInpaintAuthUploadMod.js
@@ -97,7 +97,47 @@ function addImage(url) {
     set(imagesRef, newImage);
 }
 
+function uploadImageToFirebaseStorage() {
+    //get the image from the canvas
+    canvas.loadPixels();
+    let base64Image = canvas.elt.toDataURL("image/png", 1.0);
+    //upload it to firebase storage using modular syntax
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images/' + myDBID + "/" + Date.now() + ".png");
+    const uploadTask = uploadString(storageRef, base64Image, 'data_url');
+    uploadTask.on('state_changed',
+        (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
 
+            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            // switch (snapshot.state) {
+            //   case 'paused':
+            //     console.log('Upload is paused');
+            //     break;
+            //   case 'running':
+            //     console.log('Upload is running');
+            //     break;
+            // }
+        },
+        (error) => {
+            // Handle unsuccessful uploads
+        },
+        () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                addImage(downloadURL);
+            });
+        }
+    );
+
+
+
+
+}
 
 
 function subscribeToImages() {
