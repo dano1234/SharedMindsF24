83d160a1ab5232fa0bf1207ea8cee5dce5200188
Dano <dan.osullivan@nyu.edu>
2023-11-13 11:50:54 -0500

    back at school

--- Week2/OpenAI_API_Text_Image_P5/sketch.js
+++ Week2/OpenAI_API_Text_Image_P5/sketch.js
@@ -1,11 +1,12 @@
-
-const openAIProxy = "https://openai-api-proxy.glitch.me"
+const openAIProxy = "https://openai-api-proxy.glitch.me";
 
 let img;
+let feedback;
 
 function setup() {
-    createCanvas(512, 512);
-    let input_image_field = createInput("A student trying to learn how use a machine learning API");
+    let input_image_field = createInput(
+        "A student trying to learn how use a machine learning API"
+    );
     input_image_field.size(600);
     input_image_field.id("input_image_prompt");
     let button1 = createButton("AskPicture");
@@ -13,7 +14,9 @@ function setup() {
         askForImage(input_image_field.value());
     });
     createElement("br");
-    let input_field = createInput("Why should learn to use a machine learning API?");
+    let input_field = createInput(
+        "Why should learn to use a machine learning API?"
+    );
     input_field.id("input_prompt");
     input_field.size(600);
     let button2 = createButton("AskWords");
@@ -21,20 +24,21 @@ function setup() {
         askForWords(input_field.value());
     });
     createElement("br");
-
-
-    setupAudio()
+    feedback = createP("");
+    createElement("br");
+    feedback.position(10, 90)
+    let canvas = createCanvas(512, 512);
+    canvas.position(0, 120);
+    setupAudio();
 }
 
 function draw() {
     if (img) image(img, 0, 0);
 }
 
-
 async function askForWords(p_prompt) {
     document.body.style.cursor = "progress";
-    const textDiv = select("#resulting_text");
-    textDiv.html("Waiting for reply from OpenAi...");
+    feedback.html("Waiting for reply from OpenAi...");
     const data = {
         model: "text-davinci-003",
         prompt: p_prompt,
@@ -48,50 +52,49 @@ async function askForWords(p_prompt) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Accept: 'application/json',
+            Accept: "application/json",
         },
         body: JSON.stringify(data),
     };
-    const url = openAIProxy + "/AskOpenAI/"
+    const url = openAIProxy + "/AskOpenAI/";
     console.log("words url", url, "words options", options);
     const response = await fetch(url, options);
     console.log("words_response", response);
     const openAI_json = await response.json();
     console.log("openAI_json", openAI_json);
     if (openAI_json.choices.length == 0) {
-        textDiv.html("Something went wrong, try it again");
+        feedback.html("Something went wrong, try it again");
     } else {
         let choicesjoin = "";
         for (let i = 0; i < openAI_json.choices.length; i++) {
             choicesjoin += openAI_json.choices[i].text;
         }
-        textDiv.html(choicesjoin);
+        feedback.html(choicesjoin);
         //console.log("proxy_said", proxy_said.output.join(""));
     }
     document.body.style.cursor = "auto";
 }
 
-
 async function askForImage(p_prompt) {
     document.body.style.cursor = "progress";
-    const textDiv = select("#resulting_text");
-    textDiv.html("Waiting for reply from OpenAi...");
+
+    feedback.html("Waiting for reply from OpenAi...");
     const data = {
-        "prompt": "A cute baby sea horse",
-        "n": 2,
-        "response_format": "b64_json",
-        "size": "1024x1024"
+        prompt: p_prompt,
+        n: 2,
+        response_format: "b64_json",
+        size: "512x512",
     };
     console.log("Asking Images From OpenAI via Proxy", data);
     let options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Accept: 'application/json',
+            Accept: "application/json",
         },
         body: JSON.stringify(data),
     };
-    const url = openAIProxy + "/AskOpenAIImage/"
+    const url = openAIProxy + "/AskOpenAIImage/";
     console.log("words url", url, "words options", options);
     const response = await fetch(url, options);
     console.log("words_response", response);
@@ -99,79 +102,45 @@ async function askForImage(p_prompt) {
     console.log("openAI_json", openAI_json.data[0].b64_json);
     //allow cross origin loading of images
 
-
-
-    loadImage("data:image/jpeg;base64," + openAI_json.data[0].b64_json, function (incomingImage) {
-        img = incomingImage;
-        console.log("img", img);
-    });
-
-    // //javascript way of loading images
-    // var newImg = new Image;
-    // newImg.onload = function (incomingImage) {
-    //     img = incomingImage;
-    //     console.log("img", img);
-    // }
-    // newImg.src = openAI_json.data[0].url;
-
+    loadImage(
+        "data:image/jpeg;base64," + openAI_json.data[0].b64_json,
+        function (incomingImage) {
+            img = incomingImage;
+            console.log("img", img);
+        }
+    );
 
     document.body.style.cursor = "auto";
 }
 
-
 async function askForAudio(audio) {
-
     document.body.style.cursor = "progress";
-    const textDiv = select("#resulting_text");
-    textDiv.html("Waiting for reply from OpenAi...");
+
+    feedback.html("Waiting for reply from OpenAi Audio...");
     //read test.m4a file audio file from disk
     //https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
 
+    console.log("Asking Audio From OpenAI via Proxy");
 
+    const formData = new FormData();
+    formData.append("file", audio);
+    formData.append("model", "whisper-1");
 
-    const data = {
-        "model": "whisper-1",
-        "file": audio,
-        // "prompt": "hey there",
-        // "response_format": "json",
-        // "temperature": 0.5
-    };
-    console.log("Asking Images From OpenAI via Proxy", data);
-    let options = {
+    const url = openAIProxy + "/askOpenAIAudio/";
+    console.log("audio url", url);
+
+    const response = await fetch(url, {
+        mode: "cors",
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-    const url = openAIProxy + "/askOpenAIAudio/"
-    console.log("words url", url, "words options", options);
-    const response = await fetch(url, options);
-    console.log("words_response", response);
-    const openAI_json = await response.json();
-    console.log("openAI_json", openAI_json.data[0].b64_json);
-    //allow cross origin loading of images
-
-
-
-    loadImage("data:image/jpeg;base64," + openAI_json.data[0].b64_json, function (incomingImage) {
-        img = incomingImage;
-        console.log("img", img);
+        body: formData,
     });
 
-    // //javascript way of loading images
-    // var newImg = new Image;
-    // newImg.onload = function (incomingImage) {
-    //     img = incomingImage;
-    //     console.log("img", img);
-    // }
-    // newImg.src = openAI_json.data[0].url;
-
+    const openAI_json = await response.json();
+    console.log("audio_response", openAI_json.transcription);
+    feedback.html(openAI_json.transcription);
 
     document.body.style.cursor = "auto";
 }
-
 
 function setupAudio() {
     // Set up the audio context and media stream
@@ -180,48 +149,57 @@ function setupAudio() {
     let mediaRecorder;
 
     // Get the start and stop recording buttons
-    const startButton = document.createElement('button');
-    startButton.id = 'start-recording';
-    startButton.textContent = 'Start Recording';
+    const startButton = document.createElement("button");
+    startButton.id = "start-recording";
+    startButton.textContent = "Start Recording";
+    startButton.style.position = "absolute";
+    startButton.style.top = "50px";
+    startButton.style.left = "10px";
     document.body.appendChild(startButton);
 
-    const stopButton = document.createElement('button');
-    stopButton.id = 'stop-recording';
-    stopButton.textContent = 'Stop Recording';
+    const stopButton = document.createElement("button");
+    stopButton.style.position = "absolute";
+    stopButton.style.top = "50px";
+    stopButton.style.left = "160px";
+    stopButton.id = "stop-recording";
+    stopButton.textContent = "Stop Recording";
     document.body.appendChild(stopButton);
 
-
-
     // Add a click event listener to the start recording button
-    startButton.addEventListener('click', async function () {
+    startButton.addEventListener("click", async function () {
         // Request access to the user's microphone
         mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
+        let mrChunks = [];
+
         // Create a media recorder and start recording
         mediaRecorder = new MediaRecorder(mediaStream);
+        mediaRecorder.addEventListener("dataavailable", (event) => {
+            mrChunks.push(event.data);
+        });
+        mediaRecorder.addEventListener("stop", (event) => {
+            const recordedData = new Blob(mrChunks, { type: "audio/webm" });
+            console.log("Recording stopped", recordedData);
+
+            let av = document.createElement("VIDEO");
+            var audioURL = window.URL.createObjectURL(recordedData);
+            av.src = audioURL;
+            av.width = 100;
+            av.height = 20;
+            document.body.appendChild(av);
+            av.play();
+            console.log(av);
+
+            askForAudio(recordedData);
+        });
         mediaRecorder.start();
 
-        console.log('Recording started');
+        console.log("Recording started");
     });
 
     // Add a click event listener to the stop recording button
-    stopButton.addEventListener('click', async function () {
+    stopButton.addEventListener("click", async function () {
         // Stop the media recorder and get the recorded data
         mediaRecorder.stop();
-        const recordedData = await getRecordedData(mediaRecorder);
-
-        console.log('Recording stopped', recordedData);
-        askForAudio(recordedData)
-
-
     });
-
-    // Function to get the recorded data from the media recorder
-    function getRecordedData(mediaRecorder) {
-        return new Promise(resolve => {
-            mediaRecorder.addEventListener('dataavailable', event => {
-                resolve(event.data);
-            });
-        });
-    }
 }
