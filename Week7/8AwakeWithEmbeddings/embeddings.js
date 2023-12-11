9b57732a8a9940bfa63404a520f402b61b505933
Dano <dan.osullivan@nyu.edu>
2023-12-11 17:07:08 -0500

    umap cluster analysis

--- Week7/8AwakeWithEmbeddings/embeddings.js
+++ Week7/8AwakeWithEmbeddings/embeddings.js
@@ -3,25 +3,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
 import { getDatabase, ref, onValue, set, push, onChildAdded, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+//umap-js.min.js
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 
 let db;
-let appName = "360BGReplicateGenFirebase";
+let appName = "EmbeddingClusters";
+let distanceFromCenter = 200;
 
 let camera3D, scene, renderer;
 
 const replicateProxy = "https://replicate-api-proxy.glitch.me"
 let images = [];
 let in_front_of_you;
+let myPrompts = [];
 
-initWedInterface();
+initWebInterface();
 init3D();
 initFirebase();
 
-function initWedInterface() {
+function initWebInterface() {
+
+    fetch('prompts.json')
+        .then(response => response.json())
+        .then(prompts => {
+            myPrompts = prompts.allPrompts;
+
+        })
+
     var input_image_field = document.createElement("input");
     input_image_field.type = "text";
     input_image_field.id = "input_image_prompt";
@@ -62,6 +76,142 @@ function initWedInterface() {
     ThreeJSContainer.style.width = "100%";
     ThreeJSContainer.style.height = "100%";
     document.body.append(ThreeJSContainer);
+
+    let button = document.createElement("button");
+    button.innerHTML = "Add Prompt to Database";
+    button.style.position = "absolute";
+    button.style.top = "10%";
+    button.style.left = "50%";
+    button.style.transform = "translate(-50%, -50%)";
+    button.style.fontSize = "20px";
+    button.style.zIndex = "200";
+    button.addEventListener("click", function () {
+        let dataForReplicate = "";
+        for (let i = 0; i < myPrompts.length; i++) {
+            dataForReplicate += myPrompts[i] + "\n";
+            //let prompt = myPrompts[i];
+            // let dataToSet = {
+            //     prompt: prompt,
+            // }
+            // //console.log("dataToSet", dataToSet);
+            // push(ref(db, appName + '/prompts/'), dataToSet);
+        }
+        askForEmbeddings(dataForReplicate)
+    });
+    document.body.append(button);
+
+
+
+}
+
+async function askForEmbeddings(p_prompt) {
+    let promptInLines = p_prompt.replace(/,/g, "\n");
+    let data = {
+        version: "75b33f253f7714a281ad3e9b28f63e3232d583716ef6718f2e46641077ea040a",
+        input: {
+            inputs: promptInLines,
+        },
+    };
+    console.log("Asking for Embedding Similarities From Replicate via Proxy", data);
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    const url = replicateProxy + "/create_n_get/";
+    console.log("url", url, "options", options);
+    const raw = fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            let embeddingsAndPrompts = data.output;
+            console.log("embeddingsAndPrompts", embeddingsAndPrompts);
+            let embeddings = [];
+            for (let i = 0; i < embeddingsAndPrompts.length; i++) {
+                embeddings.push(embeddingsAndPrompts[i].embedding);
+            }
+            let fittings = runUMAP(embeddings);
+            for (let i = 0; i < embeddingsAndPrompts.length; i++) {
+                placeImage(embeddingsAndPrompts[i].input, fittings[i]);
+                //embeddingsAndPrompts[i].embedding = fittings[i];
+            }
+        });
+
+    //console.log("raw", raw);
+    //const proxy_said = await raw.json();
+    //let output = proxy_said.output;
+    //console.log("Proxy Returned", output);
+    // distances = []
+    // let firstOne = output[0];
+    // for (let i = 0; i < output.length; i++) {
+    //     let thisOne = output[i];
+    //     let cdist = cosineSimilarity(firstOne.embedding, thisOne.embedding);
+    //     distances.push({ "reference": firstOne.input, "phrase": thisOne.input, "distance": cdist })
+    //     console.log(firstOne.input, thisOne.input, cdist);
+    // }
+}
+
+
+
+
+function placeImage(text, pos) {
+    console.log("placeImage", pos);
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let size = 256;
+    canvas.height = size;
+    canvas.width = size;
+    let textParts = text.split(" ");
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = '12px Arial';
+    ctx.fillStyle = 'black';
+
+
+    for (let i = 0; i < textParts.length; i++) {
+        const metrics = ctx.measureText(textParts[i]);
+        ctx.fillText(textParts[i], canvas.width / 2 - metrics.width / 2, 10 + i * 12);
+        //text(textParts[i], 10, 10 + i * 20);
+    }
+    //ctx.drawImage(img, 0, 0);
+    //let teture = new THREE.TextureLoader().load(img);
+    let texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+    //console.log(img, texture);
+    var material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+
+    var geo = new THREE.PlaneGeometry(size, size);
+    var mesh = new THREE.Mesh(geo, material);
+    mesh.position.x = pos[0] * distanceFromCenter - distanceFromCenter / 2;
+    mesh.position.y = pos[1] * distanceFromCenter - distanceFromCenter / 2;
+    mesh.position.z = pos[2] * distanceFromCenter;
+    //console.log("mesh.position", mesh.position);
+    mesh.lookAt(0, 0, 0);
+    //mesh.scale.set(10,10, 10);
+    scene.add(mesh);
+    images.push({ "object": mesh, "texture": texture });
+}
+
+
+
+
+
+function runUMAP(embeddings) {
+
+    let umap = new UMAP({
+        nNeighbors: 4,
+        minDist: .05,
+        nComponents: 3,
+        random: Math.random,
+        spread: 1,
+        //distanceFn: 'cosine',
+    });
+    const fitting = umap.fit(embeddings);
+    console.log("fitting", fitting);
+    return fitting;
+
 }
 
 
@@ -145,7 +295,7 @@ function initFirebase() {
 function subscribeToImages() {
     const commentsRef = ref(db, appName + '/images/');
     onChildAdded(commentsRef, (data) => {
-        console.log("added", data.val().location);
+        console.log("added", data.val());
         var incomingImage = new Image();
         incomingImage.crossOrigin = "anonymous";
         incomingImage.onload = function () {
@@ -209,35 +359,13 @@ function init3D() {
 
 function getPositionInFrontOfCamera() {
     const posInWorld = new THREE.Vector3();
-    in_front_of_you.position.set(0, 0, -600);  //base the the z position on camera field of view
+    in_front_of_you.position.set(0, 0, -distanceFromCenter);  //base the the z position on camera field of view
     in_front_of_you.getWorldPosition(posInWorld);
     return posInWorld;
 }
 
 
-function placeImage(img, pos) {
-    console.log("placeImage", pos);
-    let canvas = document.createElement('canvas');
-    let ctx = canvas.getContext('2d');
-    canvas.height = img.naturalHeight;
-    canvas.width = img.naturalWidth;
-    ctx.drawImage(img, 0, 0);
-    //let teture = new THREE.TextureLoader().load(img);
-    let texture = new THREE.Texture(canvas);
-    texture.needsUpdate = true;
-    //console.log(img, texture);
-    var material = new THREE.MeshBasicMaterial({ map: texture, transparent: false });
 
-    var geo = new THREE.PlaneGeometry(512, 512);
-    var mesh = new THREE.Mesh(geo, material);
-    mesh.position.x = pos.x;
-    mesh.position.y = pos.y;
-    mesh.position.z = pos.z;
-    mesh.lookAt(0, 0, 0);
-    //mesh.scale.set(10,10, 10);
-    scene.add(mesh);
-    images.push({ "object": mesh, "texture": texture });
-}
 
 
 function animate() {
