84399ded0a0a824c220b5e1823e9a7ac6305b8a6
Dano <dan.osullivan@nyu.edu>
2024-02-04 14:37:46 -0500

    manual move object

--- Week 03 Objects/2DragAndNavWLibraries/dragLibrary.js
+++ Week 03 Objects/2DragAndNavWLibraries/dragLibrary.js
@@ -1,9 +1,10 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.160.1/three.module.min.js';
-//import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
 import { DragControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/DragControls.js';
-let camera, scene, renderer;
+let camera, scene, renderer, cube;
 let dragControls;
-let objects = [];
+let orbitControls;
+let draggableObjects = [];
 initHTML();
 init3D();
 
@@ -15,10 +16,8 @@ function init3D() {
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    ///document.body.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
-    //this puts the three.js stuff in a particular div
-    document.getElementById('THREEcontainer').appendChild(renderer.domElement)
 
     let bgGeometery = new THREE.SphereGeometry(1000, 60, 40);
     // let bgGeometery = new THREE.CylinderGeometry(725, 725, 1000, 10, 10, true)
@@ -30,27 +29,25 @@ function init3D() {
     let back = new THREE.Mesh(bgGeometery, backMaterial);
     scene.add(back);
 
-    moveCameraWithMouse();
-    dragControls = new DragControls(objects, camera, renderer.domElement);
-    camera.position.z = 0;
+
+    // moveCameraWithMouse();
+    dragControls = new DragControls(draggableObjects, camera, renderer.domElement);
+    orbitControls = new OrbitControls(camera, renderer.domElement);
+
+    camera.position.z = .1;
     animate();
 }
 
 function animate() {
+    orbitControls.update();
+    for (let i = 0; i < draggableObjects.length; i++) {
+        draggableObjects[i].lookAt(camera.position);
+    }
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
 
 function initHTML() {
-    const THREEcontainer = document.createElement("div");
-    THREEcontainer.setAttribute("id", "THREEcontainer");
-    document.body.appendChild(THREEcontainer);
-    THREEcontainer.style.position = "absolute";
-    THREEcontainer.style.top = "0";
-    THREEcontainer.style.left = "0";
-    THREEcontainer.style.width = "100%";
-    THREEcontainer.style.height = "100%";
-    THREEcontainer.style.zIndex = "1";
 
     const textInput = document.createElement("input");
     textInput.setAttribute("type", "text");
@@ -68,8 +65,8 @@ function initHTML() {
             const inputRect = textInput.getBoundingClientRect();
 
             let mouse = { x: inputRect.left, y: inputRect.top };
-            console.log("Entered Text", mouse.z);
-            const pos = find3DCoornatesInFrontOfCamera(150 - camera.fov, mouse);
+            console.log("Entered Text", textInput.value);
+            const pos = find3DCoornatesInFrontOfCamera(100, mouse);
             createNewText(textInput.value, pos);
         }
     });
@@ -99,9 +96,6 @@ function initHTML() {
             reader.readAsDataURL(files[i]);
         }
     }, true);
-
-
-
 }
 
 function find3DCoornatesInFrontOfCamera(distance, mouse) {
@@ -144,7 +138,7 @@ function createNewImage(img, posInWorld, file) {
     mesh.lookAt(0, 0, 0);
     mesh.scale.set(10, 10, 10);
     scene.add(mesh);
-    objects.push(mesh);
+    draggableObjects.push(mesh);
 }
 
 function createNewText(text_msg, posInWorld) {
@@ -174,73 +168,7 @@ function createNewText(text_msg, posInWorld) {
     mesh.lookAt(0, 0, 0);
     mesh.scale.set(10, 10, 10);
     scene.add(mesh);
-    objects.push(mesh);
-}
-
-
-
-/////MOUSE STUFF
-
-let mouseDownX = 0, mouseDownY = 0;
-let lon = -90, mouseDownLon = 0;
-let lat = 0, mouseDownLat = 0;
-let isUserInteracting = false;
-
-
-function moveCameraWithMouse() {
-    //set up event handlers
-    const div3D = document.getElementById('THREEcontainer');
-    div3D.addEventListener('mousedown', div3DMouseDown, false);
-    div3D.addEventListener('mousemove', div3DMouseMove, false);
-    div3D.addEventListener('mouseup', div3DMouseUp, false);
-    div3D.addEventListener('wheel', div3DMouseWheel, { passive: true });
-    window.addEventListener('resize', onWindowResize, false);
-    //document.addEventListener('keydown', onDocumentKeyDown, false);
-    camera.target = new THREE.Vector3(0, 0, 0);  //something for the camera to look at
-}
-
-function div3DMouseDown(event) {
-    mouseDownX = event.clientX;
-    mouseDownY = event.clientY;
-    mouseDownLon = lon;
-    mouseDownLat = lat;
-    isUserInteracting = true;
-}
-
-
-function div3DMouseMove(event) {
-    if (isUserInteracting) {
-        lon = (mouseDownX - event.clientX) * 0.1 + mouseDownLon;
-        lat = (event.clientY - mouseDownY) * 0.1 + mouseDownLat;
-        computeCameraOrientation();
-    }
-}
-
-function div3DMouseUp(event) {
-    isUserInteracting = false;
-}
-
-function div3DMouseWheel(event) {
-    camera.fov += event.deltaY * 0.05;
-    camera.fov = Math.max(5, Math.min(100, camera.fov)); //limit zoom
-    camera.updateProjectionMatrix();
-}
-
-function computeCameraOrientation() {
-    lat = Math.max(- 30, Math.min(30, lat));  //restrict movement
-    let phi = THREE.MathUtils.degToRad(90 - lat);  //restrict movement
-    let theta = THREE.MathUtils.degToRad(lon);
-    //move the target that the camera is looking at
-    camera.target.x = 100 * Math.sin(phi) * Math.cos(theta);
-    camera.target.y = 100 * Math.cos(phi);
-    camera.target.z = 100 * Math.sin(phi) * Math.sin(theta);
-    camera.lookAt(camera.target);
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    console.log('Resized');
+    mesh.lookAt(0, 0, 0);
+    draggableObjects.push(mesh);
 }
 
