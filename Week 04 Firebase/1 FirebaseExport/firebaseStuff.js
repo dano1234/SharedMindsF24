ef017aed53c66bbb71dc51569b59b23d1427dd70
Dano <dan.osullivan@nyu.edu>
2024-02-10 13:18:09 -0500

    drag drop model import export firebase

--- Week 04 Firebase/1 FirebaseExport/firebaseStuff.js
+++ Week 04 Firebase/1 FirebaseExport/firebaseStuff.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getDatabase, ref, onValue, update, set, push, onChildAdded, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
-import { reactToFirebase } from './objectsFB.js';
+import { reactToFirebase } from './main.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAvM1vaJ3vcnfycLFeb8RDrTN7O2ToEWzk",
