1e04ca32d256250a9657b1945b8a4c92db17d698
Dano <dan.osullivan@nyu.edu>
2024-03-08 07:50:31 -0500

    w

--- Week 06 ML APIs/3 Individual P5 Tests/ReplicateGenFIrebase/3DSDFirebase.js
+++ Week 06 ML APIs/3 Individual P5 Tests/ReplicateGenFIrebase/3DSDFirebase.js
@@ -2,7 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-import { getDatabase, ref, onValue, set, push, onChildAdded, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+//import { getDatabase, ref, onValue, set, push, onChildAdded, onChildChanged, onChildRemoved } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
