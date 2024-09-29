632c6e3f2943351484579a641ca20869c5806c08
Dano <dan.osullivan@nyu.edu>
2024-09-29 19:36:33 -0400

    working on timing

--- Various/FaceGan/startOver.js
+++ Various/FaceGan/startOver.js
@@ -3,6 +3,9 @@ let maskGraphics;
 let masked = false;
 let video;
 let people = [];
+let readyForRequest = true;
+let whoseTurn = 0;
+const GPUServer = "https://dano.ngrok.dev/";
 
 let bodyPoseOptions = {
     modelType: "MULTIPOSE_LIGHTNING", // "MULTIPOSE_LIGHTNING", "SINGLEPOSE_LIGHTNING", or "SINGLEPOSE_THUNDE"
@@ -30,18 +33,27 @@ function setup() {
 }
 
 function draw() {
-    // background(230, 30, 23, 255);
-    //if (!masked) gradientMaskIt(staticImage);
-    //image(staticImage, 0, 0);
-    // gradientMaskIt(video);
-    // image(video, 0, 0);
+    doTurnTaking();
     for (let person of people) {
         person.drawMe();
     }
 }
 
+async function doTurnTaking() {
+    if (readyForRequest && people.length > 0) {
+        whoseTurn++;
+        if (whoseTurn >= people.length) {
+            whoseTurn = 0;
+        }
+        readyForRequest = false
+        await people[whoseTurn].ask();
+        readyForRequest = true;
+    }
+}
+
+
 function bodyPoseVideoResults(poses) {
-    background(255);
+    background(255, 255, 255, 50);
 
     //make a new person if there is a new person posing
     if (poses.length > people.length) {
@@ -116,7 +128,7 @@ function gradientMaskIt(img) {
 
     const sX = maskGraphics.width / 2;
     const sY = maskGraphics.height / 2;
-    const sR = maskGraphics.width / 4;
+    const sR = maskGraphics.width / 5;
     const eX = maskGraphics.width / 2;
     const eY = maskGraphics.height / 2;
     const eR = maskGraphics.height / 2;
@@ -150,6 +162,7 @@ class Person {
         this.matched = false;
         this.underImage = image;
         this.alterEgoImage = null;
+        this.imageForAI = null;
         this.underFrameRect = { left: 0, top: 0, width: 0, height: 0, cx: -9000, cy: -9000, headAngle: 0 };
         this.alterEgoFrameRect = null;
         this.lastUpdate = millis();
@@ -158,19 +171,65 @@ class Person {
     getCropMaskUnderImage(frameRect, incomingImage, poseNum) {
         this.lastUpdate = millis();
         this.underFrameRect = frameRect;
-        this.underImage = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
-        this.underImage = gradientMaskIt(this.underImage);//better way to get an image from a p5.Graphics?
+        this.imageForAI = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
+        this.underImage = gradientMaskIt(this.imageForAI);//better way to get an image from a p5.Graphics?
         this.poseNum = poseNum;
     }
 
+    dealWithImageFromAI(newImage) {
+        let thisPose = bodyPose.detect(newImage, function (poses) {
+            console.log("poses after inspecting return from AI", poses)
+            if (poses.length > 0) {
+                let thisFrameRect = getRect(poses[0]);
+                console.log("got Frame Rect of image from AI", thisFrameRect)
+                currentPerson.getCropMaskAlterEgoImage(thisFrameRect, newImage);
+            } else {
+                console.log("didn't find a bodyPos poses in the image from AI");
+            }
+        });
+    }
+
     getCropMaskAlterEgoImage(frameRect, incomingImage) {
-        this.alterEgoframeRect = frameRect;
+        this.alterEgoFrameRect = frameRect;
         this.alterEgoImage = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
         this.alterEgoImage = gradientMaskIt(this.alterEgoImage);//better way to get an image from a p5.Graphics?
     }
 
     isNotUpdating() {
         return (millis() - this.lastUpdate) > 1000;
+    }
+
+    async ask() {
+        console.log("asking for person ");
+        let imgBase64 = this.imageForAI.canvas.toDataURL("image/jpeg", 1.0);
+        imgBase64 = imgBase64.split(",")[1];
+        let postData = {
+            image: imgBase64,
+            faceRect: this.frameRect,
+        };
+
+        let url = GPUServer + "locateImage/";
+        const options = {
+            headers: {
+                "Content-Type": `application/json`,
+            },
+            method: "POST",
+            body: JSON.stringify(postData), //p)
+        };
+        console.log("Locating My Image ");
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log("result", result);
+        if (result.error) {
+            console.log("Error", result.error);
+            return;
+        }
+        let currentPerson = this;
+        currentPerson.latents = result.latents;
+        loadImage(result.b64Image, async function (newImage,) {
+            currentPerson.alterEgoImage = newImage;
+            dealWithImageFromAI(newImage);
+        });
     }
 
     drawMe() {
@@ -182,15 +241,15 @@ class Person {
             alterEgoGraphics.imageMode(CENTER);
             alterEgoGraphics.clear()
             //this.alterEgoGraphics.translate(this.faceRect.width * faceBorderFactor / 2, this.faceRect.width * faceBorderFactor / 2);
-            alterEgoGraphics.translate(this.alterEgoGraphics.width / 2, this.alterEgoGraphics.height / 2);
+            alterEgoGraphics.translate(alterEgoGraphics.width / 2, alterEgoGraphics.height / 2);
             alterEgoGraphics.rotate(this.underFrameRect.headAngle);
 
             // this.alterEgoGraphics.tint(255, 210);
             alterEgoGraphics.image(this.alterEgoImage, 0, 0);
-            image(this.alterEgoGraphics, this.alterEgoFrameRect.left, this.alterEgoFrameRect.top, this.alterEgoFrameRect.width, this.alterEgoFrameRect.height);
+            image(alterEgoGraphics, this.alterEgoFrameRect.left, this.alterEgoFrameRect.top, this.alterEgoFrameRect.width, this.alterEgoFrameRect.height);
             alterEgoGraphics.remove();
         } else if (this.underImage) {
-            console.log("drawing under person");
+            //console.log("drawing under person");
             image(this.underImage, this.underFrameRect.left, this.underFrameRect.top, this.underFrameRect.width, this.underFrameRect.height);
         }
         text(this.poseNum + "-P", this.underFrameRect.cx, this.underFrameRect.cy);
