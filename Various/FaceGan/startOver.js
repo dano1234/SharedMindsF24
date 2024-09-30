75e69560f30c010d20f4916c6f9f11769bd848f1
Daniel O'Sullivan <dan.osullivan@nyu.edu>
2024-09-30 13:50:15 -0400

    flip

--- Various/FaceGan/startOver.js
+++ Various/FaceGan/startOver.js
@@ -7,6 +7,7 @@ let readyForRequest = true;
 let whoseTurn = 0;
 const GPUServer = "https://dano.ngrok.dev/";
 
+
 let bodyPoseOptions = {
     modelType: "MULTIPOSE_LIGHTNING", // "MULTIPOSE_LIGHTNING", "SINGLEPOSE_LIGHTNING", or "SINGLEPOSE_THUNDE"
     enableSmoothing: true,
@@ -33,10 +34,25 @@ function setup() {
 }
 
 function draw() {
+
+    push();
+
+    // Scale -1, 1 means reverse the x axis, keep y the same.
+    scale(-1, 1);
+
+    tint(255, 20);
+    image(video, -width, 0, width, height);
+    //background(255, 255, 255, 50);
+    tint(255, 255);
     doTurnTaking();
     for (let person of people) {
         person.drawMe();
     }
+
+    // Because the x-axis is reversed, we need to draw at different x position.
+    //image(img, -width, 0);
+
+    pop();
 }
 
 async function doTurnTaking() {
@@ -53,7 +69,6 @@ async function doTurnTaking() {
 
 
 function bodyPoseVideoResults(poses) {
-    background(255, 255, 255, 50);
 
     //make a new person if there is a new person posing
     if (poses.length > people.length) {
@@ -95,7 +110,6 @@ function bodyPoseVideoResults(poses) {
         closestPerson.getCropMaskUnderImage(thisFrameRect, video, poseNum);
     }
 
-
 }
 
 function getRect(pose) {
@@ -111,19 +125,20 @@ function getRect(pose) {
 
     let top = int(centerY - faceWidth / 2);
     let left = int(centerX - faceWidth / 2);
-    let headAngle = Math.atan2(wDiff, hDiff);
+    let headAngle = Math.atan2(hDiff, wDiff) + PI;
 
     faceRect = { left: left, top: top, width: faceWidth, height: faceWidth };
     //do I nee faceRect?  //maybe for inserting face into a face
     const border = faceWidth / 5;
-    frameRect = { left: left - border, top: top - border, width: faceWidth + border * 2, height: faceWidth + border * 2, cx: pose.nose.x, cy: pose.nose.y, headAngle: headAngle };
+    frameRect = { border: border, faceRect: faceRect, left: left - border, top: top - border, width: faceWidth + border * 2, height: faceWidth + border * 2, cx: pose.nose.x, cy: pose.nose.y, headAngle: headAngle };
     return frameRect;
 }
 
-function gradientMaskIt(img) {
-    maskGraphics = createGraphics(img.width, img.height);
+function gradientMaskIt(inputImg, outputImg) {
+    outputImg = inputImg.get();
+    maskGraphics = createGraphics(inputImg.width, inputImg.height);
     maskGraphics.noStroke();
-    //maskGraphics.clear(0, 0, img.width, img.height);
+    //maskGraphics.clear(0, 0, inputImg.width, inputImg.height);
     //maskGraphics.angleMode(DEGREES);
 
     const sX = maskGraphics.width / 2;
@@ -151,9 +166,9 @@ function gradientMaskIt(img) {
     // maskGraphics.stroke(0, 244, 0);
     maskGraphics.noStroke();
     //maskGraphics.rect(0, 0, maskGraphics.width, maskGraphics.height);
-    img.mask(maskGraphics);
+    outputImg.mask(maskGraphics);
     maskGraphics.remove();
-    return img;
+    return outputImg;
 }
 
 class Person {
@@ -162,7 +177,7 @@ class Person {
         this.matched = false;
         this.underImage = image;
         this.alterEgoImage = null;
-        this.imageForAI = null;
+        this.imageWithoutMask = null;
         this.underFrameRect = { left: 0, top: 0, width: 0, height: 0, cx: -9000, cy: -9000, headAngle: 0 };
         this.alterEgoFrameRect = null;
         this.lastUpdate = millis();
@@ -171,28 +186,32 @@ class Person {
     getCropMaskUnderImage(frameRect, incomingImage, poseNum) {
         this.lastUpdate = millis();
         this.underFrameRect = frameRect;
-        this.imageForAI = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
-        this.underImage = gradientMaskIt(this.imageForAI);//better way to get an image from a p5.Graphics?
+        this.imageWithoutMask = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
+        this.underImage = gradientMaskIt(this.imageWithoutMask, this.underImage);//better way to get an image from a p5.Graphics?
         this.poseNum = poseNum;
     }
 
-    dealWithImageFromAI(newImage) {
-        let thisPose = bodyPose.detect(newImage, function (poses) {
-            console.log("poses after inspecting return from AI", poses)
-            if (poses.length > 0) {
-                let thisFrameRect = getRect(poses[0]);
-                console.log("got Frame Rect of image from AI", thisFrameRect)
-                currentPerson.getCropMaskAlterEgoImage(thisFrameRect, newImage);
-            } else {
-                console.log("didn't find a bodyPos poses in the image from AI");
-            }
-        });
+    async dealWithImageFromAI(newImage) {
+        this.getCropMaskAlterEgoImage(null, newImage)
+        // let thisPose = await bodyPose.detect(newImage, function (poses) {
+        //     console.log("poses after inspecting return from AI", poses)
+        //     if (poses.length > 0) {
+        //         let thisFrameRect = getRect(poses[0]);
+        //         console.log("got Frame Rect of image from AI", thisFrameRect)
+        //         this.getCropMaskAlterEgoImage(thisFrameRect, newImage);
+        //     } else {
+        //         console.log("didn't find a bodyPos poses in the image from AI");
+        //     }
+        // });
     }
 
     getCropMaskAlterEgoImage(frameRect, incomingImage) {
-        this.alterEgoFrameRect = frameRect;
-        this.alterEgoImage = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
-        this.alterEgoImage = gradientMaskIt(this.alterEgoImage);//better way to get an image from a p5.Graphics?
+        console.log("got image from AI", frameRect, incomingImage);
+        //this.alterEgoFrameRect = frameRect;
+
+        //this.alterEgoImage = incomingImage.get(frameRect.left, frameRect.top, frameRect.width, frameRect.height);
+        //this.alterEgoImage = gradientMaskIt(this.alterEgoImage);//better way to get an image from a p5.Graphics?
+        this.alterEgoImage = gradientMaskIt(incomingImage, this.alterEgoImage);//better way to get an image from a p5.Graphics?
     }
 
     isNotUpdating() {
@@ -201,7 +220,7 @@ class Person {
 
     async ask() {
         console.log("asking for person ");
-        let imgBase64 = this.imageForAI.canvas.toDataURL("image/jpeg", 1.0);
+        let imgBase64 = this.imageWithoutMask.canvas.toDataURL("image/jpeg", 1.0);
         imgBase64 = imgBase64.split(",")[1];
         let postData = {
             image: imgBase64,
@@ -217,19 +236,22 @@ class Person {
             body: JSON.stringify(postData), //p)
         };
         console.log("Locating My Image ");
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log("result", result);
-        if (result.error) {
-            console.log("Error", result.error);
-            return;
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log("result", result);
+            if (result.error) {
+                console.log("Error", result.error);
+                return;
+            }
+            let currentPerson = this;
+            currentPerson.latents = result.latents;
+            loadImage(result.b64Image, async function (newImage,) {
+                currentPerson.dealWithImageFromAI(newImage);
+            });
+        } catch (e) {
+            console.log("error locating face in AI");
         }
-        let currentPerson = this;
-        currentPerson.latents = result.latents;
-        loadImage(result.b64Image, async function (newImage,) {
-            currentPerson.alterEgoImage = newImage;
-            dealWithImageFromAI(newImage);
-        });
     }
 
     drawMe() {
@@ -246,13 +268,17 @@ class Person {
 
             // this.alterEgoGraphics.tint(255, 210);
             alterEgoGraphics.image(this.alterEgoImage, 0, 0);
-            image(alterEgoGraphics, this.alterEgoFrameRect.left, this.alterEgoFrameRect.top, this.alterEgoFrameRect.width, this.alterEgoFrameRect.height);
+            let b = this.underFrameRect.border;
+            image(alterEgoGraphics, this.underFrameRect.left - b - width, this.underFrameRect.top - 2 * b, this.underFrameRect.width + 2 * b, this.underFrameRect.height + 2 * b);
+
+            //image(alterEgoGraphics, this.alterEgoFrameRect.left, this.alterEgoFrameRect.top, this.alterEgoFrameRect.width, this.alterEgoFrameRect.height);
             alterEgoGraphics.remove();
         } else if (this.underImage) {
             //console.log("drawing under person");
-            image(this.underImage, this.underFrameRect.left, this.underFrameRect.top, this.underFrameRect.width, this.underFrameRect.height);
+            image(this.underImage, this.underFrameRect.left - width, this.underFrameRect.top, this.underFrameRect.width, this.underFrameRect.height);
         }
-        text(this.poseNum + "-P", this.underFrameRect.cx, this.underFrameRect.cy);
+        textSize(72);
+        text(this.poseNum + "", this.underFrameRect.cx - width, this.underFrameRect.cy);
     }
 }
 
